628fb0ad3e0165fef4e78b390756ca6957de7460
sammykirigha <dkirigha18@gmail.com>
2022-06-09 07:56:52 +0300

    modified two files

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({text, onClick}) => {
   return (
-	  <button onClick={onClick} className='button w-32 text-white bg-blue-500 hover:bg-blue-600 rounded-md p-3  '>
+	  <button onClick={onClick} className='button w-auto text-white bg-blue-500 hover:bg-blue-600 rounded-md p-3  '>
 		  {text}
 	</button>
   )
--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { RiArrowRightSLine } from "react-icons/ri";
+import Button from "../components/Button";
 
 const Doctors = () => {
-  return (
-    <div>
-      <h3>all doctors</h3>
-  </div>
-  )
-}
+    const params = useLocation();
+    const pathname = params.pathname.split("/")[2];
+    return (
+        <div>
+            <div className="flex flex-row items-center justify-between mx-4">
+                <div>
+                    <h4 className="text-lg text-slate-900 font-bold">Doctors</h4>
+                    <p className="paragraph inline-flex items-center justify-between text-md uppercase text-md ">
+                        Docris{" "}
+                        <RiArrowRightSLine className="flex items-center mx-2 mt-0.5" />{" "}
+                        <span className="text-blue-700 text-md">
+                            {pathname.toUpperCase()}
+                        </span>{" "}
+                    </p>
+                </div>
+                <Button text="Add A New Doctor" />
+            </div>
+        </div>
+    );
+};
 
-export default Doctors
+export default Doctors;
