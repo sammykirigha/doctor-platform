238cc5fd28f17848cff458373a183d3c82472367
sammykirigha <dkirigha18@gmail.com>
2022-06-09 15:45:58 +0300

    added new changes

--- src/pages/Add-Doctor.jsx
+++ src/pages/Add-Doctor.jsx
@@ -3,6 +3,25 @@ import { RiArrowRightSLine } from "react-icons/ri";
 import { useLocation } from "react-router-dom";
 import Button from "../components/Button";
 import image1 from "../data/images/01.jpg";
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+const animatedComponents = makeAnimated();
+
+const departmentsOptions = [
+    { value: "Eye Care", label: "Eye Care" },
+    { value: "Gynecologist", label: "Gynecologist" },
+    { value: "Psychotherapist", label: "Psychotherapist" },
+    { value: "Orthopedic", label: "Orthopedic" },
+    { value: "Dentist", label: "Dentist" },
+    { value: "Gastrologist", label: "Gastrologist" },
+    { value: "Urologist", label: "Urologist" },
+    { value: "Neurologist", label: "Neurologist" },
+];
+
+const genderOptions = [
+    { value: "Male", label: "Male" },
+    { value: "Female", label: "Female" },
+];
 
 const AddDoctor = () => {
     const params = useLocation();
@@ -29,7 +48,7 @@ const AddDoctor = () => {
                 </div>
             </div>
             <div className="flex flex-row mt-3 w-full gap-3 rounded-md">
-                <div className="w-[60%] bg-white h-screen">
+                <div className="w-[60%] bg-white">
                     <div className="flex flex-row items-center justify-between ml-9 mr-3 mt-7 ">
                         <div className="flex flex-row items-center ">
                             <span className="rounded-full h-28 w-28 flex items-center border border-gray-300 ">
@@ -56,8 +75,8 @@ const AddDoctor = () => {
                         </div>
                     </div>
                     <div className="mx-3 mt-5">
-                        <form>
-                            <div className="flex flex-row items-center justify-between mx-6">
+                        <form className="flex flex-col">
+                            <div className="flex flex-row items-center justify-between ml-3 mb-5 gap-2">
                                 <div className="flex flex-col ">
                                     <label>First Name</label>
                                     <input
@@ -67,13 +86,97 @@ const AddDoctor = () => {
                                     />
                                 </div>
                                 <div className="flex flex-col ">
-                                    <label>First Name</label>
+                                    <label>Last Name</label>
                                     <input
                                         type="text"
-                                        placeholder="First Name:"
+                                        placeholder="Last Name:"
                                         className="w-[300px] h-10 placeholder:italic placeholder:text-slate-300 bg-white border border-slate-300 rounded-md focus:border focus:outline-none"
                                     />
                                 </div>
+                            </div>
+                            <div className="flex flex-row items-center justify-between ml-3 mb-5 gap-2">
+                                <div className="flex flex-col ">
+                                    <label>Your Email</label>
+                                    <input
+                                        type="email"
+                                        placeholder="Email address:"
+                                        className="w-[300px] h-10 placeholder:italic placeholder:text-slate-300 bg-white border border-slate-300 rounded-md focus:border focus:outline-none"
+                                    />
+                                </div>
+                                <div className="flex flex-col ">
+                                    <label>Phone no.</label>
+                                    <input
+                                        type="text"
+                                        placeholder="Phone no:"
+                                        className="w-[300px] h-10 placeholder:italic placeholder:text-slate-300 bg-white border border-slate-300 rounded-md focus:border focus:outline-none"
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex flex-row items-center justify-between ml-3 mb-5 gap-2">
+                                <div className="flex flex-col ">
+                                    <label>Departments</label>
+                                    <Select
+                                        closeMenuOnSelect={false}
+                                        components={animatedComponents}
+                                        className="w-[300px]"
+                                        isMulti
+                                        defaultValue={departmentsOptions[2]}
+                                        options={departmentsOptions}
+                                    />
+                                </div>
+                                <div className="flex flex-col ">
+                                    <label>Gender</label>
+                                    <Select
+                                        closeMenuOnSelect={false}
+                                        components={animatedComponents}
+                                        className="w-[300px]"
+                                        options={genderOptions}
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex flex-row items-center justify-between ml-3 mb-5 gap-2">
+                                <div className="flex flex-col ">
+                                    <label>Twitter</label>
+                                    <input
+                                        type="text"
+                                        placeholder="userName:"
+                                        className="w-[300px] h-10 placeholder:italic placeholder:text-slate-300 bg-white border border-slate-300 rounded-md focus:border focus:outline-none"
+                                    />
+                                </div>
+                                <div className="flex flex-col ">
+                                    <label>Facebook</label>
+                                    <input
+                                        type="text"
+                                        placeholder="userName:"
+                                        className="w-[300px] h-10 placeholder:italic placeholder:text-slate-300 bg-white border border-slate-300 rounded-md focus:border focus:outline-none"
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex flex-row items-center justify-between ml-3 mb-5 gap-2">
+                                <div className="flex flex-col ">
+                                    <label>Linkedin</label>
+                                    <input
+                                        type="text"
+                                        placeholder="userName:"
+                                        className="w-[300px] h-10 placeholder:italic placeholder:text-slate-300 bg-white border border-slate-300 rounded-md focus:border focus:outline-none"
+                                    />
+                                </div>
+                                <div className="flex flex-col ">
+                                    <label>Instagram</label>
+                                    <input
+                                        type="text"
+                                        placeholder="userName:"
+                                        className="w-[300px] h-10 placeholder:italic placeholder:text-slate-300 bg-white border border-slate-300 rounded-md focus:border focus:outline-none"
+                                    />
+                                </div>
+                            </div>
+                                <div className="w-full mb-3 ml-3 pr-3 flex flex-col ">
+                                    <label>Your Bio</label>
+                                    <textarea
+                                        type="text"
+                                        placeholder="Your Bio:"
+                                        className="w-full mr-3 h-[100px]  placeholder:italic placeholder:text-slate-300 bg-white border border-slate-300 rounded-md focus:border focus:outline-none"
+                                    />
                             </div>
                         </form>
                     </div>
