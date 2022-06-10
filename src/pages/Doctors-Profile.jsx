cc421c2f960c1176a02b335aa98962352245681c
sammykirigha <dkirigha18@gmail.com>
2022-06-10 16:18:51 +0300

    added new changes

--- src/pages/Doctors-Profile.jsx
+++ src/pages/Doctors-Profile.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { RiArrowRightSLine } from "react-icons/ri";
+import { useLocation } from "react-router-dom";
 
 const DoctorsProfile = () => {
-  return (
-	<div>Doctors-Profile</div>
-  )
-}
+    const params = useLocation();
+    const firstName = params.pathname.split("/")[1];
+    const secondName = params.pathname.split("/")[2];
+    return (
+        <div className="mx-5">
+            <div className="flex flex-row items-center justify-between">
+                <h4>Docto's Profile & Settings</h4>
+                <div>
+                    <p className="paragraph inline-flex items-center justify-between text-md uppercase text-md ">
+                        Docris{" "}
+                        <RiArrowRightSLine className="flex items-center mx-2 mt-0.5" />{" "}
+                        <span className="text-blue-700 text-md">
+                            {firstName.toUpperCase()}
+                        </span>{" "}
+                        <RiArrowRightSLine className="flex items-center mx-2 mt-0.5" />{" "}
+                        <span className="text-blue-700 text-md">
+                            {secondName.toUpperCase()}
+                        </span>{" "}
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default DoctorsProfile
+export default DoctorsProfile;
