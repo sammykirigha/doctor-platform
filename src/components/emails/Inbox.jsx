ea92564493cef58eb44f58272948d254035d6b19
sammykirigha <dkirigha18@gmail.com>
2022-06-15 08:02:08 +0300

    added an email inbox

--- src/components/emails/Inbox.jsx
+++ src/components/emails/Inbox.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import {MdOutlineRefresh} from 'react-icons/md'
+import React from "react";
+import { MdOutlineRefresh } from "react-icons/md";
+import Emails from "./Emails";
 const Inbox = () => {
-  return (
-	  <div className=' flex flex-col mt-5'>
-		  <div className='flex flex-row items-center border-b pl-5 pb-7'>
-			  <input type='checkbox' width='30px' height='30px' className="default:ring-2 mr-7 cursor-pointer" />
-			  <MdOutlineRefresh className='text-2xl cursor-pointer text-gray-500' />
-		  </div>
-	</div>
-  )
-}
+    return (
+        <div className=" flex flex-col mt-5">
+            <div className="flex flex-row items-center border-b  pb-7">
+                <input
+                    type="checkbox"
+                    width="30px"
+                    height="30px"
+                    className="default:ring-2 mr-4 cursor-pointer w-[40px] "
+                />
+                <MdOutlineRefresh className="text-2xl cursor-pointer text-gray-500" />
+            </div>
+            <div>
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => {
+                    return <Emails />;
+                })}
+            </div>
+        </div>
+    );
+};
 
-export default Inbox
+export default Inbox;
