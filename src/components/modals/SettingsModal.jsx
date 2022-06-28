b752bf4159392f509df709f3f16eb04bbd9a26a4
sammykirigha <dkirigha18@gmail.com>
2022-06-28 21:51:59 +0300

    updated the settings modal

--- src/components/modals/SettingsModal.jsx
+++ src/components/modals/SettingsModal.jsx
@@ -20,50 +20,59 @@ const SettingsModal = ({ isOpen, closeModal, message }) => {
             closeModal={closeModal}
             positionY="right"
             positionX="end"
-            width={448}
         >
-            <div className="sm:h-[665px] lg:h-[865px] w-[400px ">
-                <div className="flex items-center justify-between w-[300px">
+            <div className="sm:h-[600px]  w-[400px]  ">
+                <div className="flex items-center justify-between w-full border-b">
                     <Logo />
                     <div className="mt-2">
                         <span
-                            className="inline-flex justify-center rounded-md border border-transparent px-4 py-2 text-sm font-medium hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                            className="inline-flex justify-center rounded-md border border-transparent px-4  font-medium  focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                             onClick={closeModal}
                         >
-                            <FaTimes />
+                            <FaTimes className="text-3xl text-gray-600 cursor-pointer" />
                         </span>
                     </div>
                 </div>
-                <div className="mt-9! z-10 flex flex-col items-center justify-center">
-                    <div className="mt-3 ">
-                        <div className="cursor-pointer">
-                            <img
-                                src={image1}
-                                alt="setting"
-                                className="h-[220px] w-[330px] rounded-md drop-shadow-md mb-2"
-                            />
-                        </div>
-                        <div className="cursor-pointer">
-                            <img
-                                src={image2}
-                                alt="setting"
-                                className="h-[220px] w-[330px] rounded-md mb-2"
-                            />
-                        </div>
-                        <div className="cursor-pointer">
-                            <img
-                                src={image3}
-                                alt="setting"
-                                className="h-[220px] w-[330px] rounded-md mb-2"
-                            />
-                        </div>
-                    </div>
-                    <div className="pt-2 flex items-center justify-center">
-                        <h5 className="text-gray-500 font-semibold cursor-pointer">
-                            Landing Demos
-                        </h5>
-                    </div>
 
+                <div className=" slim-scrollbar overflow-auto h-[450px] mt-3 flex flex-col items-center justify-center border-b ">
+                    <div className="cursor-pointer">
+                        <img
+                            src={image1}
+                            alt="setting"
+                            className="h-[200px] w-[300px] rounded-md drop-shadow-md"
+                        />
+                        <div className="mt-1 mb-6 flex items-start justify-center">
+                            <h5 className="text-gray-500 font-semibold cursor-pointer text-lg">
+                                RTL Version
+                            </h5>
+                        </div>
+                    </div>
+                    <div className="cursor-pointer">
+                        <img
+                            src={image2}
+                            alt="setting"
+                            className="h-[200px] w-[300px] rounded-md mb-2"
+                        />
+                        <div className="mt-1 mb-6 flex items-center justify-center">
+                            <h5 className="text-gray-500 font-semibold cursor-pointer text-lg">
+                                Dark Version
+                            </h5>
+                        </div>
+                    </div>
+                    <div className="cursor-pointer">
+                        <img
+                            src={image3}
+                            alt="setting"
+                            className="h-[200px] w-[300px] rounded-md mb-2"
+                        />
+                        <div className="mt-1 mb-6 flex items-center justify-center">
+                            <h5 className="text-gray-500 font-semibold cursor-pointer text-lg">
+                                Landing Demos
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-9! z-10 flex flex-col items-center justify-center">
                     <div className="flex items-center justify-center mt-8">
                         <span className="h-[38px] cursor-pointer w-[38px] bg-gray-100 rounded-md flex items-center justify-center mr-3">
                             <HiOutlineShoppingCart className="h-[25px] w-[25px]" />
