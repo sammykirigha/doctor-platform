11c9326dd82762087bb0f98be1c67063a126dd67
sammykirigha <dkirigha18@gmail.com>
2022-06-30 22:31:24 +0300

    new updates

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import {BsHeart} from 'react-icons/bs'
 const Profile = () => {
     return (
-        <div>
-            <div className="flex flex-col">
-                <div className="text-slate-900 text-md">Introduction</div>
-                <p className="pt-8 text-gray-700">
+        <div className="mx-4">
+            <div className="flex flex-col mt-7 ">
+                <div className="text-slate-900 text-lg font-semibold ">Introduction:</div>
+                <p className="pt-8 text-gray-500  font-medium">
                     Web designers to occupy the space which will later be filled
                     with 'real' content. This is required when, for example, the
                     final text is not yet available. Dummy text is also known as
@@ -13,27 +13,28 @@ const Profile = () => {
                     since the 16th century.
                 </p>
             </div>
-            <div className="flex flex-col">
-                <div className="flex flex-row">
-                    <div className="text-md">Appointment List</div>
-                    <div className="text-md">Payment List</div>
+            <div className="flex flex-col mt-5">
+                <div className="flex flex-row items-center  mb-5 ">
+                    <div className="text-lg text-slate-800 font-bold ">Appointment List</div>
+                    <div className="text-lg text-slate-800 font-bold ml-64">Payment List</div>
                 </div>
-                <div className="grid grid-cols-2">
-                    <div className="flex flex-row gap-3">
-                        <div className="flex flex-row">
-                            <span>Icon</span>
-                            <div className="flex flex-col">
-                                <span>Cardiogram</span>
-                                <span>Dr.Calvin Carlo</span>
+                <div className="">
+                    <div className="flex flex-row gap-10">
+                        <div className="flex flex-row items-center justify-evenly px-4 py-2 border drop-shadow-md bg-white w-full">
+                            <BsHeart className="h-7 w-7 text-blue-600" />
+                            <div className="flex flex-col mx-2 py-3">
+                                <span className="text-lg text-slate-900 font-bold">Cardiogram</span>
+                                <span className="text-md text-slate-500">Dr.Calvin Carlo</span>
                             </div>
-                            <span>10 Dec</span>
+                            <span className="text-md text-slate-900">10 Dec</span>
                         </div>
-                        <div className="flex flex-col">
-                            <div className="flex flex-col">
-                                <span>Cardiogram</span>
-                                <span>Full bill paid</span>
+                        <div className="flex flex-row items-center justify-evenly px-4 py-2 border drop-shadow-md bg-white w-full">
+                            <BsHeart className="h-7 w-7 text-blue-600" />
+                            <div className="flex flex-col mx-2 py-3">
+                                <span className="text-lg text-slate-900 font-bold">Cardiogram</span>
+                                <span className="text-md text-slate-500">Dr.Calvin Carlo</span>
                             </div>
-                            <span>icon</span>
+                            <span className="text-md text-slate-900">10 Dec</span>
                         </div>
                     </div>
                 </div>
