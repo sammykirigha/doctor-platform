895249340608da1c099e822bba75b25c8fd2434f
sammykirigha <dkirigha18@gmail.com>
2022-07-13 21:11:09 +0300

    new updates

--- src/components/others/FooterMain.jsx
+++ src/components/others/FooterMain.jsx
@@ -6,7 +6,8 @@ import { GoLocation } from "react-icons/go";
 
 const FooterMain = () => {
     return (
-		<div className="bg-[#202942] pl-3 mt-8 pt-5 sm:pt-5 md:pt-5  h-[auto] w-full
+        <div className="bg-[#202942]">
+            <div className="bg-[#202942] pl-3 mt-8 pt-5 sm:pt-5 md:pt-5  h-[auto] w-full
 		 grid sm:grid-cols-2 sm:items-start md:grid-cols-3 md:items-start md:justify-evenly lg:pt-5 lg:px-16 lg:grid-cols-4 lg:items-start lg:justify-evenly">
             <div className="flex flex-col sm:ml-5 md:ml-10 lg:ml-0 -md:mt-10 ">
                 <div className="flex flex-row items-center mt-3">
@@ -76,10 +77,21 @@ const FooterMain = () => {
                 </div>
                 <div className="flex flex-row items-center text-gray-100 text-lg">
                     <GoLocation className="mr-2 mt-1" />
-                    <span>+152 534-468-854</span>
+                    <span>View on Google map</span>
+                </div>
+            </div>
+            </div>
+            <div className=" mx-20 my-10 border-t pt-3 flex flex-col  sm:flex-col sm:items-center sm:justify-between md:flex-row  md:items-center md:justify-between lg:flex-row lg:items-center lg:justify-between ">
+                <p className="text-gray-100 text-lg">2022 @ Dorcis. Designed by Dorcis kirigha</p>
+                <div className="flex flex-row items-center justify-between sm:mt-8 md:flex-row  md:items-center md:justify-between md:mt-0 lg:mt-0">
+                    <span className="text-gray-100 text-lg mr-2">Terms</span>
+                    <span className="text-gray-100 text-lg mr-2">Privacy</span>
+                    <span className="text-gray-100 text-lg mr-2">About</span>
+                    <span className="text-gray-100 text-lg mr-2">Contact</span>
                 </div>
             </div>
         </div>
+		
     );
 };
 
