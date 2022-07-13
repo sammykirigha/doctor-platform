1bf43b7cdcfbf43695286018b388f0e55970de58
sammykirigha <dkirigha18@gmail.com>
2022-07-13 23:23:27 +0300

    added time table responsiveness

--- src/components/others/TimeTable.jsx
+++ src/components/others/TimeTable.jsx
@@ -4,14 +4,14 @@ import {HiOutlineMail} from 'react-icons/hi'
 
 const TimeTable = () => {
     return (
-        <div className="flex flex-row w-full mb-5">
-            <div className=" border shadow-sm rounded-md w-[40%] pt-3">
+        <div className="flex flex-col sm:flex-col md:flex-col lg:flex-row mb-5">
+            <div className="sm:w-[100%] sm:mt-5 md:w-[100%] lg:w-[40%] border shadow-sm rounded-md  pt-3">
                 <div className="flex flex-row  mx-3 ">
                     <span className="flex flex-row items-center ">
                         <BsFillClockFill className="text-md text-blue-600  mr-3" />{" "}
                         Monday
                     </span>
-                    <div className="ml-[140px]  w-80">
+                    <div className="sm:ml-auto sm:w-auto ml-auto w-auto ">
                         Time:{" "}
                         <span className="text-blue-400">8.00 - 20.00</span>
                     </div>
@@ -21,7 +21,7 @@ const TimeTable = () => {
                         <BsFillClockFill className="text-md text-blue-600 mr-3" />{" "}
                         Tuesday
                     </span>
-                    <div className="ml-[140px] w-80">
+                    <div className=" sm:ml-auto sm:w-auto ml-auto w-auto">
                         Time:{" "}
                         <span className="text-blue-400">8.00 - 20.00</span>
                     </div>
@@ -31,7 +31,7 @@ const TimeTable = () => {
                         <BsFillClockFill className="text-md text-blue-600 mr-3" />{" "}
                         Wednesday
                     </span>
-                    <div className="ml-[118px] w-80">
+                    <div className=" sm:ml-auto sm:w-auto ml-auto w-auto">
                         Time:{" "}
                         <span className="text-blue-400">8.00 - 20.00</span>
                     </div>
@@ -41,7 +41,7 @@ const TimeTable = () => {
                         <BsFillClockFill className="text-md text-blue-600 mr-3" />{" "}
                         Thursday
                     </span>
-                    <div className="ml-[134px] w-80">
+                    <div className=" sm:ml-auto sm:w-auto ml-auto w-auto">
                         Time:{" "}
                         <span className="text-blue-400">8.00 - 20.00</span>
                     </div>
@@ -51,7 +51,7 @@ const TimeTable = () => {
                         <BsFillClockFill className="text-md text-blue-600 mr-3" />{" "}
                         Friday
                     </span>
-                    <div className="ml-[157px] w-80">
+                    <div className=" sm:ml-auto sm:w-auto ml-auto w-auto">
                         Time:{" "}
                         <span className="text-blue-400">8.00 - 20.00</span>
                     </div>
@@ -61,7 +61,7 @@ const TimeTable = () => {
                         <BsFillClockFill className="text-md text-blue-600 mr-3" />{" "}
                         Saturday
                     </span>
-                    <div className="ml-[140px] w-80">
+                    <div className=" sm:ml-auto sm:w-auto ml-auto w-auto">
                         Time:{" "}
                         <span className="text-blue-400">8.00 - 20.00</span>
                     </div>
@@ -71,15 +71,15 @@ const TimeTable = () => {
                         <BsFillClockFill className="text-md text-blue-600 mr-3" />{" "}
                         Sunday
                     </span>
-                    <div className="ml-[150px]">
+                    <div className=" sm:ml-auto sm:w-auto ml-auto w-auto">
                         Time:{" "}
                         <span className="text-blue-400">8.00 - 20.00</span>
                     </div>
                 </div>
             </div>
-            <div className="flex flex-row justify-between flex-1 mt-5">
-                <div className="ml-5">
-                    <div className="flex flex-col ml-32">
+            <div className="flex flex-col sm:flex-col md:flex-row lg:flex-row justify-between flex-1 mt-5">
+                <div className="ml-5 border flex flex-col py-4 items-center rounded-md">
+                    <div className="flex flex-col sm:ml-10 md:ml-10 ">
                         <span className="h-[60px] w-[60px] bg-blue-100 rounded-md flex items-center justify-center">
                             <BsTelephone className="text-2xl text-blue-800 font-bold" />
                         </span>
@@ -89,18 +89,18 @@ const TimeTable = () => {
                     </div>
 
                     <div className="flex flex-col justify-center">
-                        <p className="text-lg text-slate-900 opacity-40">
+                        <p className="text-lg text-slate-900 opacity-40 mx-3">
                             Great doctor if you need your family member to get
                             effective immediate assistance
                         </p>
-                        <span className="text-md text-blue-500 ml-20 mt-5">
+                        <span className="text-md text-blue-500 mx-auto mt-5">
                             +152 534-468-854
                         </span>
                     </div>
                 </div>
 
-                <div className="ml-5">
-                    <div className="flex flex-col ml-32">
+                <div className="ml-5 border flex flex-col py-4 items-center rounded-md">
+                    <div className="flex flex-col">
                         <span className="h-[60px] w-[60px] bg-blue-100 rounded-md flex items-center justify-center">
                             <HiOutlineMail className="text-2xl text-blue-800 font-bold" />
                         </span>
@@ -110,10 +110,10 @@ const TimeTable = () => {
                     </div>
 
                     <div className="flex flex-col justify-center">
-                        <p className="text-lg text-slate-900 opacity-40">
+                        <p className="text-lg text-slate-900 opacity-40 mx-3">
                             Great doctor if you need your family member to get effective immediate assistance
                         </p>
-                        <span className="text-md text-blue-500 ml-20 mt-5">
+                        <span className="text-md text-blue-500 mx-auto mt-5">
                             contact@example.com
                         </span>
                     </div>
