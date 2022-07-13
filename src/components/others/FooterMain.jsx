fcbc6262bbf49a979292d473b03b89542eae48b7
sammykirigha <dkirigha18@gmail.com>
2022-07-13 20:41:32 +0300

    updates

--- src/components/others/FooterMain.jsx
+++ src/components/others/FooterMain.jsx
@@ -6,10 +6,10 @@ import { GoLocation } from "react-icons/go";
 
 const FooterMain = () => {
     return (
-		<div className="bg-[#202942] pl-3 mt-8 pt-5 sm:pt-5 md:pt-5 lg:pt-0   h-[auto] w-full
-		 grid sm:grid-cols-2 sm:items-start md:grid-cols-3 md:items-start md:justify-evenly lg:grid-cols-4 lg:items-start lg:justify-evenly">
+		<div className="bg-[#202942] pl-3 mt-8 pt-5 sm:pt-5 md:pt-5  h-[auto] w-full
+		 grid sm:grid-cols-2 sm:items-start md:grid-cols-3 md:items-start md:justify-evenly lg:pt-5 lg:px-16 lg:grid-cols-4 lg:items-start lg:justify-evenly">
             <div className="flex flex-col sm:ml-5 md:ml-10 lg:ml-0 -md:mt-10 ">
-                <div className="flex flex-row items-center">
+                <div className="flex flex-row items-center mt-3">
                     <img
                         src={logo}
                         alt="logo"
@@ -25,8 +25,8 @@ const FooterMain = () => {
                     consultation.
                 </p>
             </div>
-            <div className="flex flex-col sm:ml-5 lg:ml-0 md:mt-10 ">
-                <h3 className="text-gray-100 mb-3 text-xl">Company</h3>
+            <div className="flex flex-col sm:ml-5 md:pt-0 md:mt-0 md:ml-20 lg:ml-20 ">
+                <h3 className="text-gray-100 mb-1 mt-3 text-xl">Company</h3>
                 {[
                     "About us",
                     "Services",
@@ -44,8 +44,8 @@ const FooterMain = () => {
                 })}
             </div>
 
-            <div className="flex flex-col sm:mt-10 sm:ml-5 md:pt-0 md:mt-0 md:ml-10 md:mr-0 lg:pt-0 lg:ml-0 lg:mr-0">
-                <h3 className="text-gray-100 mb-3 text-xl ">Departments</h3>
+            <div className="flex flex-col sm:mt-10 sm:ml-5 md:pt-0 md:mt-0 md:ml-10 md:mr-0">
+                <h3 className="text-gray-100 mb-1 mt-3 text-xl ">Departments</h3>
                 {[
                     "Eye Care",
                     "Psychotherapy",
@@ -64,8 +64,8 @@ const FooterMain = () => {
                 })}
             </div>
 
-            <div className="flex flex-col sm:ml-5 sm:mt-10 lg:ml-0 md:mt-10">
-                <h3 className="text-gray-100 mb-3 text-xl">Contact us</h3>
+            <div className="flex flex-col sm:ml-5 sm:mt-10 md:mt-10  lg:mt-0">
+                <h3 className="text-gray-100 mb-1 mt-3 text-xl">Contact us</h3>
                 <div className="flex flex-row items-center text-gray-100 text-lg">
                     <AiOutlineMail className="mr-2 mt-1" />
                     <span>contact@example.com</span>
