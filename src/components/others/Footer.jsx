4746c9e1da59a35d13a246132710da92e464aaf3
sammykirigha <dkirigha18@gmail.com>
2022-07-14 23:58:01 +0300

    updatings

--- src/components/others/Footer.jsx
+++ src/components/others/Footer.jsx
@@ -2,13 +2,13 @@ import React, { useContext } from 'react';
 import { AiFillHeart } from 'react-icons/ai';
 import { NavigationContext } from '../../contexts/navigation.context';
 
-// const Footer = () => {
-// 	const { sideBarOpen, toggleSidebar } = useContext(NavigationContext);
-//   return (
-// 	  <div className={` bg-white h-[55px] bg-white-600 border-t border-gray-200 ${sideBarOpen && "ml-[250px]"}`}>
-// 		  <p className='text-gray-400 font-normal  px-2 text-md inline-flex items-center mt-3'>{ new Date().getFullYear()} @ Dorcis. Design with <span className='mx-[5px] mt-[6px]'><AiFillHeart className='text-red-400 ' /></span> by SamuelKirigha </p>
-// 	</div>
-//   )
-// }
+const Footer = () => {
+	const { sideBarOpen, toggleSidebar } = useContext(NavigationContext);
+  return (
+	  <div className={` bg-white h-[55px] bg-white-600 border-t border-gray-200 ${sideBarOpen && "ml-[250px]"}`}>
+		  <p className='text-gray-400 font-normal  px-2 text-md inline-flex items-center mt-3'>{ new Date().getFullYear()} @ Dorcis. Design with <span className='mx-[5px] mt-[6px]'><AiFillHeart className='text-red-400 ' /></span> by SamuelKirigha </p>
+	</div>
+  )
+}
 
-// export default Footer
+export default Footer
