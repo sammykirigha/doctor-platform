efd93e220fa71ddb1fe37b1cdd2d339fa9726c83
sammykirigha <dkirigha18@gmail.com>
2022-09-28 17:55:31 +0300

    updated the patient profile page

--- src/components/others/ProfileSettings.jsx
+++ src/components/others/ProfileSettings.jsx
@@ -1,9 +1,10 @@
-import { Form, Formik } from "formik";
-import React from "react";
+import { ErrorMessage, Form, Formik } from "formik";
+import React, { useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import fileUploader from "../../utils/file-uploader";
+import { validateNewPassword, validatePassword } from "../../utils/validationhelper";
 import InputField from "../Authentication/InputField";
 import Button from "../common/Button";
-import DatePickerField from "./DatePicker";
 import FormSelect from "./Select";
 
 const genderOptions = [
@@ -18,8 +19,47 @@ const maritalStatusOptions = [
 
 export const ProfileSettings = () => {
     const { patient } = useSelector((state) => state.patient);
+    const [profileImage, setProfileImage] = useState("");
+    const [uploading, setUploading] = useState(false);
+
+    const imageUploadRef = useRef(null);
+    
+        const handleImageChange = async (e) => {
+        if (e.target.files?.length === 0) return;
+
+        const file = e.target.files[0];
+
+        try {
+            setUploading(true);
+
+            const url = await fileUploader(file);
+            setUploading(false);
+            setProfileImage(url);
+        } catch (error) {
+            setUploading(false);
+            console.log(error);
+        }
+
+        //url
+    };
 
     console.log("<<<<<patient>>>>>>", patient);
+
+    const onSubmit = async (values) => {
+        const inputValues = {
+            id: patient?.id,
+            firstname: values.firstname,
+            lastname: values.lastname,
+            email: values.email,
+            image: profileImage,
+            phone: values.phone,
+            address: values.address,
+            // age: values.age,
+            gender: values.gender,
+            maritalStatus: values.maritalStatus,
+            description: values.description
+        }
+    }
     return (
         <div className="mx-5">
             <div className="flex flex-col mt-5">
@@ -28,10 +68,10 @@ export const ProfileSettings = () => {
                 </div>
 
                 <div className="mt-5 mb-5">
-                    {patient.id && (
+                    {patient?.id && (
                         <Formik
                             initialValues={patient}
-                            // onSubmit={onSubmit}
+                            onSubmit={onSubmit}
                             // validationSchema={createDoctorSchema}
                         >
                             {({
@@ -48,7 +88,7 @@ export const ProfileSettings = () => {
                                     <div className="flex my-3">
                                         <div className=" flex flex-row ">
                                             <img
-                                                src="https://nellions.co.ug/wp-content/uploads/2018/06/male-placeholder-image.jpeg"
+                                                src={profileImage}
                                                 height="100px"
                                                 width="100px"
                                                 className="rounded-full drop-shadow-lg"
@@ -66,19 +106,27 @@ export const ProfileSettings = () => {
                                             </div>
                                         </div>
                                         <div className="flex flex-col gap-3">
-                                            <button className="bg-blue-600 text-white py-2 px-3 rounded-md cursor-pointer">
-                                                Upload
-                                            </button>
-                                            <button className="bg-blue-100 text-blue-600 py-2 px-3 rounded-md cursor-pointer">
-                                                Remove
-                                            </button>
+                                             <Button
+                                                onClick={() =>
+                                                    imageUploadRef?.current?.click()
+                                                }
+                                                text="Upload"
+                                                loading={uploading}
+                                            />
+                                            <input
+                                                ref={imageUploadRef}
+                                                onChange={handleImageChange}
+                                                className="hidden"
+                                                type="file"
+                                                name=""
+                                                id=""
+                                            />
                                         </div>
                                     </div>
                                     <div className="flex flex-row items-center justify-between mt-3  mb-5 gap-16">
                                         <div className="flex flex-col  w-full ">
                                             <InputField
                                                 name="firstname"
-                                                value={values.firstname}
                                                 validate=""
                                                 type="text"
                                                 label="First Name"
@@ -88,7 +136,6 @@ export const ProfileSettings = () => {
                                         <div className="flex flex-col  w-full ">
                                             <InputField
                                                 name="lastname"
-                                                value={values.lastname}
                                                 validate=""
                                                 type="text"
                                                 label="Last Name"
@@ -100,7 +147,6 @@ export const ProfileSettings = () => {
                                         <div className="flex flex-col w-full">
                                             <InputField
                                                 name="email"
-                                                value={values.email}
                                                 validate=""
                                                 type="text"
                                                 label="Email"
@@ -110,10 +156,9 @@ export const ProfileSettings = () => {
                                         <div className="flex flex-col w-full">
                                             <InputField
                                                 name="phone"
-                                                value={values.phone}
                                                 validate=""
                                                 type="text"
-                                                label="Phone"
+                                                label="Phone Num"
                                                 placeholder="Phone:"
                                             />
                                         </div>
@@ -122,7 +167,6 @@ export const ProfileSettings = () => {
                                         <div className="flex flex-col w-full">
                                             <InputField
                                                 name="address"
-                                                value={values.address}
                                                 validate=""
                                                 type="text"
                                                 label="Address"
@@ -132,7 +176,6 @@ export const ProfileSettings = () => {
                                         <div className="flex flex-col w-full">
                                             <InputField
                                                 name="county"
-                                                value={values.county}
                                                 validate=""
                                                 type="text"
                                                 label="County"
@@ -144,7 +187,6 @@ export const ProfileSettings = () => {
                                         <div className="flex flex-col w-full">
                                             <InputField
                                                 name="nationality"
-                                                value={values.nationality}
                                                 validate=""
                                                 type="text"
                                                 label="Nationality"
@@ -164,10 +206,13 @@ export const ProfileSettings = () => {
 
                                     <div className="flex flex-row items-center justify-between  mb-5 gap-16">
                                         <div className="flex flex-col w-full">
-                                            <label className="text-lg font-medium after:content-['*'] after:ml-0.5 after:text-red-500 mb-2">
-                                                Age
-                                            </label>
-                                            <input type="number" />
+                                             <InputField
+                                                name="description"
+                                                validate=""
+                                                type="text"
+                                                label="Description"
+                                                placeholder="Description:"
+                                            />
                                         </div>
                                         <div className="flex flex-col w-full">
                                             <label className="text-lg font-medium after:content-['*'] after:ml-0.5 after:text-red-500">
@@ -187,42 +232,78 @@ export const ProfileSettings = () => {
                         </Formik>
                     )}
                     <div className="flex flex-col mt-7">
-                        <h4 className="text-slate-900 text-lg font-bold">
-                            Change Password:
-                        </h4>
-                        <div className="flex flex-col mt-5 ">
-                            <label className="after:content-['*'] after:ml-0.5 after:text-red-500 font-semibold">
-                                Old Password:
-                            </label>
-                            <input
-                                type="text"
-                                placeholder="Old password"
-                                className="w-full h-10 placeholder:italic pl-2 placeholder:text-slate-300 bg-white border border-slate-300 rounded-md focus:border-0 focus:outline focus:outline-blue-600"
-                            />
-                        </div>
-                        <div className="flex flex-col mt-5 ">
-                            <label className="after:content-['*'] after:ml-0.5 after:text-red-500 font-semibold">
-                                New Password
-                            </label>
-                            <input
-                                type="text"
-                                placeholder="New password"
-                                className="w-full h-10 placeholder:italic pl-2 placeholder:text-slate-300 bg-white border border-slate-300 rounded-md focus:border-0 focus:outline focus:outline-blue-600"
-                            />
-                        </div>
-                        <div className="flex flex-col mt-5 ">
-                            <label className="after:content-['*'] after:ml-0.5 after:text-red-500 font-semibold">
-                                Re-type New Password
-                            </label>
-                            <input
-                                type="text"
-                                placeholder="Re-type New password"
-                                className="w-full h-10 placeholder:italic pl-2 placeholder:text-slate-300 bg-white border border-slate-300 rounded-md focus:border-0 focus:outline focus:outline-blue-600"
-                            />
-                        </div>
-                        <button className="px-3 py-2 bg-blue-600 text-white w-[200px] mt-10 rounded-md">
-                            Save password
-                        </button>
+                        <Formik
+                            initialValues={{
+                                password: "",
+                                newPassword: "",
+                            }}
+                            // onSubmit={}
+                        >
+                            {({
+                                values,
+                                errors,
+                                touched,
+                                handleChange,
+                                handleBlur,
+                                handleSubmit,
+                                isSubmitting,
+                            }) => (
+                                <Form>
+                                    <h4 className="text-slate-900 text-lg font-bold">
+                                        Change Password:
+                                    </h4>
+                                    <div className="flex flex-col mt-5 ">
+                                        <InputField
+                                        name="password"
+                                        validate={validatePassword}
+                                        type="password"
+                                        label="Enter Old Password:"
+                                        />
+                                        <ErrorMessage name="password">
+                                        {(error) => (
+                                            <p className="text-md text-red-600">
+                                                {" "}
+                                                {error}
+                                            </p>
+                                        )}
+                                    </ErrorMessage>
+                                    </div>
+                                    <div className="flex flex-col my-5 ">
+                                       <InputField
+                                        name="newPassword"
+                                        validate={validateNewPassword}
+                                        type="password"
+                                        label=" New Password:"
+                                    />
+
+                                    <ErrorMessage name="confirmnewPassword">
+                                        {(error) => (
+                                            <p className="text-md text-red-600">
+                                                {" "}
+                                                {error}
+                                            </p>
+                                        )}
+                                    </ErrorMessage>
+                                    </div>
+                                    {/* <div className="flex flex-col mt-5 ">
+                                        <label className="after:content-['*'] after:ml-0.5 after:text-red-500 font-semibold">
+                                            Re-type New Password
+                                        </label>
+                                        <input
+                                            type="text"
+                                            placeholder="Re-type New password"
+                                            className="w-full h-10 placeholder:italic pl-2 placeholder:text-slate-300 bg-white border border-slate-300 rounded-md focus:border-0 focus:outline focus:outline-blue-600"
+                                        />
+                                    </div> */}
+                                    
+                                <Button
+                                    type="submit"
+                                    text="Change Password"
+                                    loading={isSubmitting}
+                                />
+                                </Form>
+                            )}
+                        </Formik>
                     </div>
                 </div>
             </div>
